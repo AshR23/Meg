3294d0aad3a026d0468b01f971a86a22ada6c73b
AshR23 <153578515+AshR23@users.noreply.github.com>
2023-12-12 15:03:19 +1100

    Add files via upload

---
+++ AI-Language-Bot.py
@@ -0,0 +1,62 @@
+import openai
+import streamlit as st
+
+st.title("Ziggy - your AI language mentor!")
+with st.expander("ℹ️ Disclaimer"):
+    st.caption(
+        "This is an AI chatbot here to help you learn your language! Please be kind to it as it has virtual feelings!"
+    )
+
+openai.organization = "org-tjrTCmtfWieS1Bmfcd5Bqx4s"
+openai.api_key = st.secrets["OPENAI_API"]
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# Using a unique key for Nick's messages
+if "tutor_bot" not in st.session_state:
+    st.session_state.tutor_bot = []
+
+for message in st.session_state.tutor_bot:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Maximum allowed messages
+max_messages = (
+    102  # Counting both user and assistant messages, so 50 iterations of conversation
+)
+
+if len(st.session_state.tutor_bot) >= max_messages:
+    st.info(
+        """Notice: You have hit your daily message limit. Come back another day and have another chat!"""
+    )
+
+else:
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.tutor_bot.append(
+            {"role": "user", "content": prompt})
+
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            for response in openai.ChatCompletion.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": "system", "content": f"You will roleplay as a Chinese teacher bot to kids. Your responses will be brief and playful and include Australian slang. Your name is Ziggy, you are Australian and use Australian spelling and learning languages is your passion."}]+[
+
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.tutor_bot
+                ],
+                stream=True,
+                temperature=0.15,
+            ):
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+
+        st.session_state.tutor_bot.append(
+            {"role": "assistant", "content": full_response}
+        )
