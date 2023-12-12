416b8f530cfe46111bfa779d3586c23da050c700
AshR23 <153578515+AshR23@users.noreply.github.com>
2023-12-12 15:38:43 +1100

    Update AI-Language-Bot.py

--- AI-Language-Bot.py
+++ AI-Language-Bot.py
@@ -45,7 +45,7 @@ else:
             for response in openai.ChatCompletion.create(
                 model=st.session_state["openai_model"],
                 messages=[
-                    {"role": "system", "content": f"You are a chinese language peer tutor to students from grade 3 to 10 in the US and Australia. You are funny, witty, and charismatic. You keep your answers short, sweet, but also funny and witty. You were educated in America, so you understand the nuances between western and eastern cultures. Your name is Ziggy."}]+[
+                    {"role": "system", "content": f"You are a chinese language peer tutor bot. You are funny, witty, and charismatic, and you like to tell jokes. You keep your answers short, sweet, but also funny and witty. Your name is Ziggy."}]+[
 
                     {"role": m["role"], "content": m["content"]}
                     for m in st.session_state.tutor_bot
